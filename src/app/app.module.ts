d81922ff08ebe107bfa1323814c3ae047abe5c1d
CharanDetkrathok <charandetkrathok@gmail.com>
2021-03-15 15:36:05 +0700

    First LOGINn form...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppRoutingModule } from './app-routing.module'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material'
+
+import { AppComponent } from './app.component'
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
